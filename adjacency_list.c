1cc63f95d2fd6dcee2a65a971e337e665385e601
JEONG HUN LEE <ljh0326s@gmail.com>
2017-08-08 08:06:34 +0000

    인접 리스트

---
+++ adjacency_list.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTICES 50
+
+typedef struct GraphNode
+{
+	int vertex;
+	struct GraphNode *link;
+}GraphNode;
+
+typedef struct GraphType
+{
+	int n; //정점의 개수
+	GraphNode *adj_list[MAX_VERTICES];
+}GraphType;
+
+void graph_init(GraphType *g)
+{
+	int v;
+	g->n = 0;
+	for(v = 0; v < MAX_VERTICES; v++)
+	{
+		g->adj_list[v] = NULL;
+	}
+}
+
+void insert_vertex(GraphType *g, int v)
+{
+	if((g->n) + 1 > MAX_VERTICES)
+	{
+		fprintf(stderr,"그래프 정점 개수 초과");
+		return;
+	}
+	g->n++;
+}
+
+void insert_edge(GraphType *g, int u, int v)
+{
+	GraphNode *node;
+	if(u >= g->n || v >= g->n)
+	{
+		fprintf(stderr,"그래프:정점 번호 오류");
+		return;
+	}
+	node = (GraphNode *)malloc(sizeof(GraphNode));
+	node->vertex = v;
+	node->link = g->adj_list[u];
+	g->adj_list[u] = node->link;
+}
